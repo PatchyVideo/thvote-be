a83a7bf9f9f6ed3ad2c5492a73a7429aeac6e8d5
zyddnys <zyddnys@outlook.com>
2022-02-19 15:09:13 -0500

    add CI

--- result-query/build.sh
+++ result-query/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 NAME=thvote-result-query
-export PKG_CONFIG_ALLOW_CROSS=1
-export OPENSSL_STATIC=true
-export OPENSSL_DIR=/musl
-cargo build --target x86_64-unknown-linux-musl --release
 cp ../target/x86_64-unknown-linux-musl/release/${NAME} tmp.app
 docker build --no-cache -t ${NAME} .
 docker save -o ${NAME}.tar ${NAME}
 rm tmp.app
+mv ${NAME}.tar ../
