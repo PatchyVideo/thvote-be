675f078982ce7fc3911650b0f4d540f8706bdf9e
shirokurakana <admin@thdog.moe>
2022-05-29 19:35:26 +0800

    scraper: fix pixiv novel cover again(

--- scraper/sites/pixnovel.py
+++ scraper/sites/pixnovel.py
@@ -17,46 +17,44 @@ async def pixndata(pid: str, udid: str) -> RespBody:
         aapi = AppPixivAPI(client=client)
         await aapi.login(refresh_token=get_cache('pixiv_token'))
         info = await aapi.novel_detail(pid)
-        if info.get('error'):
-            return RespBody(status='apierr', msg=f'pixapierr: {ujson.encode(info["error"])}')
-        data = info['novel']
-        uid = data['user']['id']
-        author = f'pixiv-author:{uid}'
-
-        desc = html_to_plain_text(data['caption'])
-
-        is_touhou = False
-        bad = ''
-        bad_tags = get_cache('pixiv_bad_tags')
-        for tag in data['tags']:
-            if not is_touhou:
-                if '東方' in tag['name'] or '幻想入り' in tag['name']:
-                    is_touhou = True
-            if bad_tags and not bad:
-                if tag['name'] in bad_tags:
-                    bad = tag['name']
-            if is_touhou and bad:
-                break
-        status = 'ok'
-        msg = ''
+    if info.get('error'):
+        return RespBody(status='apierr', msg=f'pixapierr: {ujson.encode(info["error"])}')
+    data = info['novel']
+    uid = data['user']['id']
+    author = f'pixiv-author:{uid}'
+    desc = html_to_plain_text(data['caption'])
+    is_touhou = False
+    bad = ''
+    bad_tags = get_cache('pixiv_bad_tags')
+    for tag in data['tags']:
         if not is_touhou:
-            status = 'warning'
-            msg += 'may not touhou. '
-        if bad:
-            status = 'warning'
-            msg += f'bad tag: {bad}'
+            if '東方' in tag['name'] or '幻想入り' in tag['name']:
+                is_touhou = True
+        if bad_tags and not bad:
+            if tag['name'] in bad_tags:
+                bad = tag['name']
+        if is_touhou and bad:
+            break
+    status = 'ok'
+    msg = ''
+    if not is_touhou:
+        status = 'warning'
+        msg += 'may not touhou. '
+    if bad:
+        status = 'warning'
+        msg += f'bad tag: {bad}'
 
-        data = RespBody.Data(
-            title=data['title'],
-            udid=udid,
-            cover=data['image_urls']['square_medium'],
-            desc=desc,
-            ptime=get_ptime(data['create_date']),
-            author=[author],
-            author_name=[data['user']['name']],
-            tname='ARTICLE',
-        )
-        return RespBody(status=status, msg=msg, data=data)
+    data = RespBody.Data(
+        title=data['title'],
+        udid=udid,
+        cover=data['image_urls']['square_medium'].replace('i.pximg.net','i.pixiv.re'),
+        desc=desc,
+        ptime=get_ptime(data['create_date']),
+        author=[author],
+        author_name=[data['user']['name']],
+        tname='ARTICLE',
+    )
+    return RespBody(status=status, msg=msg, data=data)
 
 
 def get_ptime(create_date: str) -> str:
