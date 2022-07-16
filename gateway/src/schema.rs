979b594cfe3ab357460e86ad7cab5dca7c4cf181
zyddnys <zyddnys@outlook.com>
2022-07-16 10:38:23 -0400

    bugfix

--- gateway/src/schema.rs
+++ gateway/src/schema.rs
@@ -6,6 +6,7 @@ use chrono::{DateTime, Utc};
 
 use crate::result_query::CPRanking;
 use crate::result_query::CharacterOrMusicRanking;
+use crate::result_query::Reasons;
 use crate::submit_handler::CPSubmitGQL;
 use crate::submit_handler::CPSubmitRestQuery;
 use crate::submit_handler::CharacterSubmitGQL;
@@ -114,19 +115,19 @@ impl Query {
 	async fn queryCharacterRanking(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32) -> FieldResult<CharacterOrMusicRanking> {
 		result_query::queryCharacterRanking_impl(context, query, vote_start, vote_year).await
 	}
-	async fn queryCharacterReasons(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32, rank: i32) -> FieldResult<CharacterOrMusicRanking> {
+	async fn queryCharacterReasons(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32, rank: i32) -> FieldResult<Reasons> {
 		result_query::queryCharacterReasons_impl(context, query, vote_start, vote_year, rank).await
 	}
 	async fn queryMusicRanking(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32) -> FieldResult<CharacterOrMusicRanking> {
 		result_query::queryMusicRanking_impl(context, query, vote_start, vote_year).await
 	}
-	async fn queryMusicReasons(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32, rank: i32) -> FieldResult<CharacterOrMusicRanking> {
+	async fn queryMusicReasons(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32, rank: i32) -> FieldResult<Reasons> {
 		result_query::queryMusicReasons_impl(context, query, vote_start, vote_year, rank).await
 	}
 	async fn queryCPRanking(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32) -> FieldResult<CPRanking> {
 		result_query::queryCPRanking_impl(context, query, vote_start, vote_year).await
 	}
-	async fn queryCPReasons(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32, rank: i32) -> FieldResult<CharacterOrMusicRanking> {
+	async fn queryCPReasons(context: &Context, query: Option<String>, vote_start: DateTime<Utc>, vote_year: i32, rank: i32) -> FieldResult<Reasons> {
 		result_query::queryCPReasons_impl(context, query, vote_start, vote_year, rank).await
 	}
 }
